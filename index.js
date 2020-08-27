a44c7d20b2a648ee888ceb2136c9436b41a84cea
Klaartje Claire <rosemarieclaire@gmail.com>
2020-08-27 15:56:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten() {
+pop("Tom Poes");
+}
+//Add your functions and code here
